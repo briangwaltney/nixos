b688e3479b8f2c59ae5bda5c59e9ff4b8e5814eb
Brian Gwaltney <brian.gwaltney@gmail.com>
2025-07-21 10:11:33 -0700

    shell module

--- configuration.nix
+++ configuration.nix
@@ -79,29 +79,6 @@
     defaultEditor = true;
   };
 
-  programs.zsh = {
-    enable = true;
-    enableCompletion = true;
-    autosuggestions.enable = true;
-
-    shellAliases = let
-      flakePath = "~/.config/nixos";
-    in {
-      air = "~/go/bin/air -c .air.toml";
-      rebuild = "~/.config/rebuild.sh";
-      hms = "home-manager switch --flake ${flakePath}";
-      v = "nvim";
-    };
-
-    shellInit = ''
-      set -o emacs
-      export EDITOR="nvim"
-      export VISUAL="nvim"
-      eval "$(starship init zsh)"
-      eval "$(zoxide init zsh)"
-    '';
-  };
-
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.brian = {
     isNormalUser = true;
--- shell-config.nix
+++ shell-config.nix
@@ -9,7 +9,7 @@
   programs.starship = {
     enable = true;
     settings = {
-      format = "$all$nix_shell$nodejs$lua$golang$rust$php$git_branch$git_commit$git_state$git_status\n$username$hostname$directory";
+      format = "$directory$git_status$git_branch$line_break$character";
       directory = {
         format = "[$path ](#8FBCBB)";
         truncation_length = 12;
@@ -35,5 +35,28 @@
         vimcmd_visual_symbol = "[](bold fg:#fdba74)";
       };
     };
+  };
+
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    autosuggestions.enable = true;
+
+    shellAliases = let
+      flakePath = "~/.config/nixos";
+    in {
+      air = "~/go/bin/air -c .air.toml";
+      rebuild = "~/.config/rebuild.sh";
+      hms = "home-manager switch --flake ${flakePath}";
+      v = "nvim";
+    };
+
+    shellInit = ''
+      set -o emacs
+      export EDITOR="nvim"
+      export VISUAL="nvim"
+      eval "$(starship init zsh)"
+      eval "$(zoxide init zsh)"
+    '';
   };
 }
