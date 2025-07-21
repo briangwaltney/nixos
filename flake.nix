f90d3c8a162f0ec7689fd3bac0275191922b2f30
Brian Gwaltney <brian.gwaltney@gmail.com>
2025-07-20 21:44:16 -0700

    added home-manager

--- flake.nix
+++ flake.nix
@@ -4,24 +4,32 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    # home-manager = {
-    #   url = "github:nix-community/home-manager";
-    #   inputs.nixpkgs.follows = "nixpkgs";
-    # };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
     self,
     nixpkgs,
+    home-manager,
     ...
-  } @ inputs: {
+  } @ inputs: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      # specialArgs = {inherit inputs;};
+      inherit system;
+      specialArgs = {inherit inputs;};
       modules = [
         ./newconfig.nix
-        # inputs.home-manager.nixosModules.default
       ];
+
+      homeConfigurations."brian" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [./home.nix];
+      };
     };
   };
 }
---
+++ home.nix
@@ -0,0 +1,24 @@
+{
+  config,
+  pkgs,
+  ...
+}: {
+  home.username = "brian";
+  home.homeDirectory = "/home/brian";
+
+  home.stateVersion = "24.11";
+
+  home.packages = [
+    pkgs.nerdfonts
+  ];
+
+  home.file = {
+  };
+
+  home.sessionVariables = {
+    EDITOR = "neovim";
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
